f9f34bd30b3bd06d381deb02e8c2a49a47675d67
Crowdbotics <team@crowdbotics.com>
2023-08-06 11:33:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "forex-course-42682",
+  url: "https://forex-course-42682.botics.co",
+  api: "https://forex-course-42682.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
